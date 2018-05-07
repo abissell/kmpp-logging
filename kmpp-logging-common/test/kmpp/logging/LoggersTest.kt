0c2602f2aca8721e129f1a8ea72d0e2e1afe321d
Andrew Bissell <abissell@gmail.com>
2018-05-07 06:05:48 +0000

    construct JVM unit test using redirected stderr

--- kmpp-logging-common/test/kmpp/logging/LoggersTest.kt
+++ kmpp-logging-common/test/kmpp/logging/LoggersTest.kt
@@ -7,21 +7,17 @@ private val logger = initTopLevelLogger {}
 
 class LoggersTest {
     private class LoggerContainer {
-        companion object : Log() {
-            const val SOME_TEST_VAL = "42"
-        }
+        companion object : Log()
 
         val instanceLogger = logger
         fun error(msg: String) = logger.error { msg }
     }
 
     @Test fun printLoggerExamples() {
-        println("Running the test!")
         val loggerContainer1 = LoggerContainer()
         val loggerContainer2 = LoggerContainer()
-        loggerContainer1.error(LoggerContainer.SOME_TEST_VAL)
-        loggerContainer2.error(LoggerContainer.SOME_TEST_VAL)
         assertSame(loggerContainer1.instanceLogger, loggerContainer2.instanceLogger)
-        logger.error { "topLevelLogger" }
+        loggerContainer1.error("companion object")
+        logger.error { "top level" }
     }
 }
--- kmpp-logging-jvm/test/kmpp/logging/JvmLoggersTest.kt
+++ kmpp-logging-jvm/test/kmpp/logging/JvmLoggersTest.kt
@@ -1,9 +1,38 @@
 package kmpp.logging
 
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 import kotlin.test.Test
+import kotlin.test.assertEquals
+
+private val topLevelLogger = initTopLevelLogger {}
 
 class JvmLoggersTest {
+    companion object : Log()
+
     @Test fun printLoggerExamples() {
-        println("Running the test!")
+        // Print some output from the companion object and top level logger to stderr, and
+        // capture in a PrintStream
+        val baos = ByteArrayOutputStream()
+        val ps = PrintStream(baos)
+        val oldErr = System.err
+        System.setErr(ps)
+        logger.error { "companion object" }
+        topLevelLogger.error { "top level" }
+        System.err.flush()
+        System.setErr(oldErr)
+
+        // Validate that the log message prefixes match the expected
+        val classStr =
+            this::class.toString()
+                .removePrefix("class ")
+                .removeSuffix(" (Kotlin reflection is not available)")
+        val outputStrs = baos.toString().split('\n')
+        assertEquals(getLoggerPrefix(outputStrs[0]), classStr)
+        assertEquals(getLoggerPrefix(outputStrs[1]), classStr)
     }
+
+    private fun getLoggerPrefix(msg: String): String =
+            msg.substringBeforeLast(" - ")
+                .substringAfterLast("ERROR ")
 }
